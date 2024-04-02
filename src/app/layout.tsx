8d97ec39d5551d7d089f8bcdad7063bf1dd0f8ff
Yaroslav <shipilov@umich.edu>
2024-04-01 19:36:22 -0700

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Merriweather&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/bff.svg" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
