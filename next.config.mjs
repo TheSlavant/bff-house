73ce356a8d789fb0da43b881bd4bbd32d33b6975
Yaroslav <shipilov@umich.edu>
2024-04-02 18:51:22 -0700

    fix next.js bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    swcMinify: true,
+    experimental: {
+        serverMinification: false,
+    },
+};
 
 export default nextConfig;
